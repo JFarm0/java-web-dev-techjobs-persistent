9c91323d50d101ea68b4beab39f99e173afb9c44
J Farmer <JFarm0>
2021-07-08 19:38:48 -0500

    Finished and Reviewed!

--- queries.sql
+++ queries.sql
@@ -11,6 +11,7 @@ WHERE location = "St. Louis City";
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
-SELECT name, description
+SELECT DISTINCT name, description
 FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
 ORDER BY name ASC;
